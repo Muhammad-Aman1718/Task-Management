e9c9f0a92640dfa8d285c8ff8548fc178ac7bda5
Muhammad-Aman1718 <ag3734359@gmail.com>
2026-06-07 21:56:31 +0500

    f

--- src/components/buttons/Button.tsx
+++ src/components/buttons/Button.tsx
@@ -36,7 +36,7 @@ const styles = StyleSheet.create({
     shadowColor: '#6C3BFF',
     shadowOffset: { width: 0, height: 0 },
     shadowOpacity: 0.35,
-    shadowRadius: 1,
+    shadowRadius: 0,
 
     elevation: 10, // Android glow
 
